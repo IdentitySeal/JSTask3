4248b5f18efbffa78cfefb1b351e5308b9b83933
IdentitySeal <teemealeheen@gmail.com>
2020-04-19 13:13:32 +0100

    Js Third Task

---
+++ app.js
@@ -0,0 +1,27 @@
+function checkNum(num) {
+  let numData = [];
+  for (i = 1; i <= num; i++) {
+
+    if (i % 2 === 0 && i % 3 === 0 && i % 5 === 0) {
+      numData.push("yu-gi-oh");
+    } else if (i % 2 === 0 && i % 3 === 0) {
+      numData.push("yu-gi");
+    } else if (i % 2 === 0 && i % 5 === 0) {
+      numData.push("yu-oh");
+    } else if (i % 3 === 0 && i % 5 === 0) {
+      numData.push("gi-oh");
+    } else if (i % 2 === 0) 
+      numData.push("yu");
+    } else if (i % 3 === 0) {
+      numData.push("gi");
+    } else if (i % 5 === 0) {
+      numData.push("oh");
+    } else {
+      numData.push(i);
+    }
+  }
+  console.log(numData);
+    return numData;
+}
+checkNum(100);
+checkNum(55);
